dc96352ce6146046684b54155bf533e599ffc467
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-04-28 20:57:36 +0000

    Added some detail to the profile page, commiting early due to bug

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,35 +3,29 @@ from .models import Order, OrderLineItem
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
-    """
-    Order Line Item Admin Inline
-    """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
-# Register your models here.
 class OrderAdmin(admin.ModelAdmin):
-    """
-    Order Admin
-    """
     inlines = (OrderLineItemAdminInline,)
-    readonly_fields = ('order_number', 'date',
-                      'delivery_cost', 'order_total',
-                      'grand_total', 'original_bag',
-                      'stripe_pid',)
 
-    fields = ('order_number', 'date', 'full_name',
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
+
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
               'order_total', 'grand_total', 'original_bag',
-              'stripe_pid',)
-    
+              'stripe_pid')
+
     list_display = ('order_number', 'date', 'full_name',
-                    'order_total', 'delivery_cost', 'grand_total',)
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
 
     ordering = ('-date',)
-
 
 admin.site.register(Order, OrderAdmin)
